080d3e7bef5e77d7f723e6f80c1fa7ad0fee063c
@maxim_pechenkin <maxim_pechenkin@genemer.com>
2022-05-19 17:14:23 +0700

    add pages add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ProfilePage from "./Components/ProfilePage";
+import MainPage from "./Components/MainPage";
+import Blog from "./Components/Blog";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to={'/profile'}> ProfilePage </Link>
+        <Link to={'/'}> Main page </Link>
+        <Link to={'/blog'}> Blog </Link>
       </header>
+      <Routes>
+        <Route path={'/'} element={<MainPage/>}/>
+        <Route path={'/profile'} element={<ProfilePage/>}/>
+        <Route path={'/blog'} element={<Blog/>}/>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+// <ProfilePage/>
+// <MainPage/>
+// <Blog/>
